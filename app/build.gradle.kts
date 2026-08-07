e34caeb789a1885bc0c69baba9f9d9ed347f7b06
i <36800583+Bl0ck154@users.noreply.github.com>
2026-08-07 23:02:19 +0300

    Bump stable toggle fix to v0.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bl0ck154.ninebotblocker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.7.0"
+        versionCode = 13
+        versionName = "0.7.1"
     }
 
     signingConfigs {
