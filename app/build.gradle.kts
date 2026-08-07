656f1bb37db90e5db48b6cbbf0c956edc3214ece
i <36800583+Bl0ck154@users.noreply.github.com>
2026-08-07 21:14:29 +0300

    Bump diagnostic scan fix to v0.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bl0ck154.ninebotblocker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.5.1"
+        versionCode = 9
+        versionName = "0.5.2"
     }
 
     signingConfigs {
