a2e879638d251c4502f3b4cddc252d0a315363be
i <36800583+Bl0ck154@users.noreply.github.com>
2026-08-07 14:24:02 +0300

    Test Ninebot crypto counter transition and PING frame

--- app/src/test/java/com/bl0ck154/ninebotblocker/NinebotCryptoTest.java
+++ app/src/test/java/com/bl0ck154/ninebotblocker/NinebotCryptoTest.java
@@ -1,6 +1,7 @@
 package com.bl0ck154.ninebotblocker;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -10,6 +11,22 @@ public final class NinebotCryptoTest {
         NinebotCrypto crypto = new NinebotCrypto("NBScooter2020");
         byte[] encrypted = crypto.encrypt(NinebotProtocol.initPacket());
         assertArrayEquals(hex("5AA500F565B968000046FF0000"), encrypted);
+        assertEquals(1, crypto.counter());
+    }
+
+    @Test
+    public void pingUsesNextCounterAfterInit() {
+        NinebotCrypto crypto = new NinebotCrypto("NBScooter2020");
+        crypto.encrypt(NinebotProtocol.initPacket());
+        crypto.setBleData(hex("000102030405060708090A0B0C0D0E0F"));
+
+        byte[] appKey = hex("101112131415161718191A1B1C1D1E1F");
+        byte[] encryptedPing = crypto.encrypt(NinebotProtocol.pingPacket(appKey));
+
+        assertEquals(2, crypto.counter());
+        assertArrayEquals(
+                hex("5AA51073AE1E8FDC35483AC359641C16B0CD872F7C5D581303B2690002"),
+                encryptedPing);
     }
 
     private static byte[] hex(String value) {
