89d436babc65d5d12acf03e7b560bd9f0c782bbd
i <36800583+Bl0ck154@users.noreply.github.com>
2026-08-08 13:24:50 +0300

    Bump feature release to 0.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.bl0ck154.ninebotblocker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.9.3"
+        versionCode = 21
+        versionName = "0.10.0"
     }
 
     signingConfigs {
