337912d3c79425a9136bef1f4848f297fe375857
i <36800583+Bl0ck154@users.noreply.github.com>
2026-08-08 08:21:06 +0300

    Bump companion release to 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.bl0ck154.ninebotblocker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.8.1"
+        versionCode = 17
+        versionName = "0.9.0"
     }
 
     signingConfigs {
