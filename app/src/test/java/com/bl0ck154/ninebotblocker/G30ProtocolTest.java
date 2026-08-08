44ebe990d2af71cf007598f84d8eebe1e7ca7a18
i <36800583+Bl0ck154@users.noreply.github.com>
2026-08-08 17:37:08 +0300

    Update telemetry tests after trip cleanup

--- app/src/test/java/com/bl0ck154/ninebotblocker/G30ProtocolTest.java
+++ app/src/test/java/com/bl0ck154/ninebotblocker/G30ProtocolTest.java
@@ -20,14 +20,12 @@ public class G30ProtocolTest {
         ScooterTelemetry t = new ScooterTelemetry();
         assertTrue(G30Protocol.applyTelemetryPacket(response(G30Protocol.REG_BATTERY, le16(73)), t));
         assertTrue(G30Protocol.applyTelemetryPacket(response(G30Protocol.REG_SPEED, le16(273)), t));
-        assertTrue(G30Protocol.applyTelemetryPacket(response(G30Protocol.REG_TRIP, le16(1240)), t));
         assertTrue(G30Protocol.applyTelemetryPacket(response(G30Protocol.REG_RANGE, le16(5850)), t));
         assertTrue(G30Protocol.applyTelemetryPacket(response(G30Protocol.REG_CONTROLLER_TEMP, le16(315)), t));
         assertTrue(G30Protocol.applyTelemetryPacket(response(G30Protocol.REG_ODOMETER, le32(12_481_000L)), t));
 
         assertEquals(Integer.valueOf(73), t.getBatteryPercent());
         assertEquals(27.3, t.getSpeed(), 0.001);
-        assertEquals(12.4, t.getTripDistance(), 0.001);
         assertEquals(58.5, t.getRemainingRange(), 0.001);
         assertEquals(31.5, t.getControllerTemperature(), 0.001);
         assertEquals(12_481.0, t.getTotalDistance(), 0.001);
