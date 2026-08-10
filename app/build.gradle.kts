5acb8e03b1a3c2a1fcb6ceb7edf6cb6c1150fac4
i <36800583+Bl0ck154@users.noreply.github.com>
2026-08-10 19:13:53 +0300

    Bump app version to 0.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.bl0ck154.ninebotblocker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.11.1"
+        versionCode = 26
+        versionName = "0.12.0"
     }
 
     signingConfigs {
